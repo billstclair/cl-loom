18ccc81441c607b4f6947862c6367cbf1774e686
Bill St. Clair <billstclair@gmail.com>
2011-08-23 10:53:07 -0400

    Export recently added accessors. Get rid of cl-user garbage symbols.

--- source/package.lisp
+++ source/package.lisp
@@ -22,159 +22,161 @@
         :anaphora)
   (:nicknames lm)
   ;; Shadow conflicting closer-mop symbols before :use
-  #.(nconc '(:shadowing-import-from closer-mop)
-           (conflicting-symbols-between :cl :closer-mop))
-  (:shadowing-import-from alexandria lastcar)
-  (:import-from cl-user conflicting-symbols-between)
-  (:export c-escape
-           c-unescape
+  #.`(:shadowing-import-from :closer-mop
+                             ,@(conflicting-symbols-between :cl :closer-mop))
+  (:shadowing-import-from :alexandria #:lastcar)
+  (:import-from :cl-user #:conflicting-symbols-between)
+  (:export #:c-escape
+           #:c-unescape
 
-           *loom-server*
-           with-loom-server
-           with-loom-transaction
+           #:*loom-server*
+           #:with-loom-server
+           #:with-loom-transaction
 
-           loom-server
-           base-uri-of
-           setup-of
-           make-configuration
-           make-loom-server
+           #:loom-server
+           #:base-uri-of
+           #:setup-of
+           #:make-configuration
+           #:make-loom-server
 
-           loom-server-setup
-           make-loom-server-setup
-           loom-server-setup-host
-           loom-server-port
-           loom-server-path
-           loom-server-use-ssl-p
-           loom-server-config-dir
-           loom-server-binary-pathname
+           #:loom-server-setup
+           #:make-loom-server-setup
+           #:loom-server-setup-host
+           #:loom-server-port
+           #:loom-server-path
+           #:loom-server-use-ssl-p
+           #:loom-server-config-dir
+           #:loom-server-binary-pathname
 
-           initialize-server
-           loom-client-error
-           loom-client-error-args
-           get-loom-error-property
+           #:initialize-server
+           #:loom-client-error
+           #:loom-client-error-args
+           #:get-loom-error-property
 
-           parse-kv
-           kv-lookup
-           alist-to-kv-string
-
-           loom-loc
-           loom-hash
-
-           grid-buy
-           grid-sell
-           *zero*
-           grid-issuer
-           grid-touch
-           grid-vacant-p
-           random-vacant-grid-loc
-           grid-look
-           grid-move
-           grid-scan
-           create-asset
-           destroy-asset
-
-           archive-request
-           archive-buy
-           archive-sell
-           archive-touch
-           archive-vacant-p
-           random-vacant-archive-loc
-           archive-look
-           archive-write
-
-           random-loc
-           sha256-function
-           sha256
-           grid-hash
-           archive-hash
-           format-loom-loc
-           loom-loc-xor
-           fold-hash
-           string-to-hex
-           parse-hex
-           print-hex
-           make-asset-description
+           #:parse-kv
+           #:kv-lookup
+           #:alist-to-kv-string
            
-           loom-passphrase
-           passphrase-location
-           get-wallet
-           get-private-wallet
-           wallet
-           wallet-assets
-           wallet-locations
-           wallet-recording-p
-           wallet-history
-           wallet-properties
-           wallet-get-property
-           asset
-           make-asset
-           asset-name
-           asset-scale
-           asset-precision
-           asset-id
-           location
-           make-location
-           location-name
-           location-loc
-           history
-           make-history
-           history-hash
-           history-time
-           history-qty
-           history-type
-           history-loc
-           history-memo
-           find-asset
-           find-asset-by-id
-           find-location
-
-           initialize-usage-issuer
-           initialize-new-store
-           create-wallet
-           move-wallet
-
-           add-asset
-           remove-asset
-
-           format-loom-qty
-           unformat-loom-qty
-
+           #:loom-loc
+           #:loom-hash
+           
+           #:grid-buy
+           #:grid-sell
+           #:*zero*
+           #:grid-issuer
+           #:grid-touch
+           #:grid-vacant-p
+           #:random-vacant-grid-loc
+           #:grid-look
+           #:grid-move
+           #:grid-scan
+           #:create-asset
+           #:destroy-asset
+           
+           #:archive-request
+           #:archive-buy
+           #:archive-sell
+           #:archive-touch
+           #:archive-vacant-p
+           #:random-vacant-archive-loc
+           #:archive-look
+           #:archive-write
+           
+           #:random-loc
+           #:sha256-function
+           #:sha256
+           #:grid-hash
+           #:archive-hash
+           #:format-loom-loc
+           #:loom-loc-xor
+           #:fold-hash
+           #:string-to-hex
+           #:parse-hex
+           #:print-hex
+           #:make-asset-description
+           
+           #:loom-passphrase
+           #:passphrase-location
+           #:get-wallet
+           #:get-private-wallet
+           #:wallet
+           #:wallet-assets
+           #:wallet-locations
+           #:wallet-recording-p
+           #:wallet-history
+           #:wallet-properties
+           #:wallet-get-property
+           #:asset
+           #:make-asset
+           #:asset-name
+           #:asset-scale
+           #:asset-precision
+           #:asset-id
+           #:location
+           #:make-location
+           #:location-name
+           #:location-loc
+           #:location-disabled-p
+           #:location-wallet-p
+           #:history
+           #:make-history
+           #:history-hash
+           #:history-time
+           #:history-qty
+           #:history-type
+           #:history-loc
+           #:history-memo
+           #:find-asset
+           #:find-asset-by-id
+           #:find-location
+           
+           #:initialize-usage-issuer
+           #:initialize-new-store
+           #:create-wallet
+           #:move-wallet
+           
+           #:add-asset
+           #:remove-asset
+           
+           #:format-loom-qty
+           #:unformat-loom-qty
+           
            ;; loom-objects.lisp
-           loom-persist
+           #:loom-persist
+           #:loom-store
 
-           loom-store
            ;; accessors
-           root-loc-of
-           usage-loc-of
+           #:root-loc-of
+           #:usage-loc-of
            
-           with-loom-store
-           *root-location-file*
-           save-root-location-file
-           make-loom-store
-           *max-linked-node-length*
-           *minimum-linked-node-fill*
-           read-location-dependencies
-           *loom-in-string*
-           read-from-location
-           *loom-out*
-           write-to-location
-           define-standard-readers
-           define-standard-writers
-           loom-equality-test
-           determine-class
-           ensure-loom-class
-           *force-loom-load*
-           load-loom-instance
-           persist-loom-slots
-           *id*
-           map-loom-instances-of-class
-           do-loom-instances-of-class
-           get-instance-id
-           class-persisted-slots
-           remove-loom-object
-           loom-garbage-collect
-          
-           wallet-loom-store
+           #:with-loom-store
+           #:*root-location-file*
+           #:save-root-location-file
+           #:make-loom-store
+           #:*max-linked-node-length*
+           #:*minimum-linked-node-fill*
+           #:read-location-dependencies
+           #:*loom-in-string*
+           #:read-from-location
+           #:*loom-out*
+           #:write-to-location
+           #:define-standard-readers
+           #:define-standard-writers
+           #:loom-equality-test
+           #:determine-class
+           #:ensure-loom-class
+           #:*force-loom-load*
+           #:load-loom-instance
+           #:persist-loom-slots
+           #:*id*
+           #:map-loom-instances-of-class
+           #:do-loom-instances-of-class
+           #:get-instance-id
+           #:class-persisted-slots
+           #:remove-loom-object
+           #:loom-garbage-collect
+           
+           #:wallet-loom-store
            ))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
